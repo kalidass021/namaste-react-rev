427b843ecb13fda683b0a48cea70e68324f07801
Kalidass <kalidass.k021@gmail.com>
2024-10-20 22:39:38 +0530

    create header component

--- App.jsx
+++ App.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = () => {
+
+/* 
+* Header
+    - logo
+    - nav items
+* Body
+    - search
+    - Restaurant container
+        - Restaurant card
+* Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
+
+const Header = () => {
     return (
-        <div>
-            <h1>This is Title...</h1>
+        <div className='header'>
+
         </div>
     )
 }
 
 const App = () => {
-
-    const name = 'Kalidass';
     return (
-        <div>
-        <Title />
-        <h1>{name}</h1>
-        <h1>App</h1>
+        <div className='app'>
+
         </div>
     )
 }
