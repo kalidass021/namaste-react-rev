3e9d09f05e76ed7c1450534aafc96d5f4e3b9434
Kalidass <kalidass.k021@gmail.com>
2024-10-25 11:52:26 +0530

    implement top rated restaurants using useState

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,35 @@
-import resList from "../utils/mockData";
-import RestaurantCard from "./RestaurantCard";
+import { useState } from 'react';
+
+import resList from '../utils/mockData';
+import RestaurantCard from './RestaurantCard';
+import '../../index.css';
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  // let listOfRestaurants = [...resList];
+  console.log('listOfRestaurants before', listOfRestaurants);
   return (
     <div className='body'>
-      <div className='search'>Search</div>
+      <div
+        className='filter'
+        onClick={() => {
+          // filter logic here
+          const filteredList = listOfRestaurants.filter(
+            (res) => res.info.avgRating > 4
+          );
+          setListOfRestaurants(filteredList);
+          console.log('resList after filter', filteredList);
+        }}
+      >
+        <button className='filter-btn'>Top Rated</button>
+      </div>
       <div className='res-container'>
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
 };
-
 
 export default Body;
