2a0f263210648a99245504da862c54846c4227a6
Kalidass <kalidass.k021@gmail.com>
2024-10-28 12:24:07 +0530

    use state concept

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,32 @@
-import { LOGO_URL } from "../utils/constants";
+import { useState } from 'react';
+import { LOGO_URL } from '../utils/constants';
+import '../../index.css';
 
 const Header = () => {
-    return (
-      <div className='header'>
-        <div>
-          <img
-            className='logo'
-            src={LOGO_URL}
-          />
-        </div>
-        <div className='nav-items'>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btnName, setBtnName] = useState('login');
+  return (
+    <div className='header'>
+      <div>
+        <img className='logo' src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className='login'
+            onClick={() => {
+              btnName === 'login' ? setBtnName('logout') : setBtnName('login');
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
