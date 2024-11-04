f58f0ee51c51d47011fe221f752fb1250b5c5820
Kalidass <kalidass.k021@gmail.com>
2024-11-04 13:19:50 +0530

    implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
 import '../index.css';
 
 /* 
@@ -25,11 +29,33 @@ const App = () => {
   return (
     <div className='app'>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Body />
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { LOGO_URL } from '../utils/constants';
 import '../../index.css';
 
@@ -11,9 +12,9 @@ const Header = () => {
       </div>
       <div className='nav-items'>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
           <li>Cart</li>
           <button
             className='login'
